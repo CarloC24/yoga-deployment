4ed2f4accd3ac517807dc38d8c53811618b02c75
CarloC24 <carlo.clamucha@gmail.com>
2019-05-20 12:16:37 -0500

    added fixes

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,11 +1,11 @@
-const { GraphQLServer } = require('graphql-yoga');
-const Mutation = require('./resolvers/Mutation');
-const Query = require('./resolvers/Query');
-const db = require('./db');
+const { GraphQLServer } = require("graphql-yoga");
+const Mutation = require("./resolvers/Mutation");
+const Query = require("./resolvers/Query");
+const db = require("./db");
 
 function createServer() {
   return new GraphQLServer({
-    typeDefs: 'src/schema.graphql',
+    typeDefs: "src/schema.graphql",
     resolvers: {
       Mutation,
       Query
--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,8 @@
 //CONNECTS TO THE REMOTE PRISMA DB and gives us the ability to run JS on our database
 const { Prisma } = require("prisma-binding"); // CONNECTING THE DATABASE ON TO THE PRISMA SERVER;
+const grsphql = require("./generated/prisma.graphql");
 const db = new Prisma({
-  typeDefs: "./generated/prisma.graphql",
+  typeDefs: "src/generated/prisma.graphql",
   endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET,
   debug: false //console logs all your mutations and queries
